bc3ca4b5c33967fe92cca7af450d0dfd52e6a85f
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-03-25 16:45:24 +0100

    [add] getApi

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,38 @@
 <script>
+import axios from 'axios';
 import Main from './components/Main.vue';
 import Header from './components/Header.vue';
+import {store} from './data/store';
 export default{
   components:{
     Header,
     Main
+  },
+
+  data(){
+    return{
+      store
+    }
+  },
+
+  methods:{
+    getApi(){
+      axios.get(this.store.apiUrl, {
+        params:{
+          page: 2
+        }
+      })
+      .then(result =>{
+        console.log(result.data);
+      })
+      .catch(error =>{
+        console.log(error);
+      })
+    }
+  },
+
+  mounted(){
+    this.getApi();
   }
 }
 </script>
--- src/data/store.js
+++ src/data/store.js
@@ -2,5 +2,5 @@
 import {reactive} from 'vue';
 
 export const store = reactive({
-    
+    apiUrl: 'https://rickandmortyapi.com/api/character'
 })
